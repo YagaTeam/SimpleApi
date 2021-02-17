f68337f4a2bc0423f5082a3c1b2d8c5404b08462
RuslanSadykov <phaenir@hotmail.com>
2021-02-17 14:34:41 +0100

    Initial Commit

---
+++ src/SimpleApi/Controllers/NameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : Controller
+    {
+        private static readonly string[] Names = new[] {
+            "Ruslan", "Olga", "Ivan", "Sofia"
+        };
+        [HttpGet]
+        public IActionResult Get() => Ok(Names);
+        [HttpGet]
+        public IActionResult Get(int id) => id < Names.Length ? Ok(Names[id]) : NotFound();
+    }
+}
---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApi.Controllers;
+using Xunit;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        NameController controller = new NameController();
+        [Fact]
+        public void GetReturnMyName()
+        {
+            var actionResult = controller.Get(0) as OkObjectResult;
+            Assert.IsType<OkObjectResult>(actionResult);
+            Assert.Equal(StatusCodes.Status200OK, actionResult.StatusCode);
+            Assert.Equal("Ruslan", actionResult.Value as string);
+        }
+        [Fact]
+        public void GetFailedReturn()
+        {
+            var actionResult = controller.Get(10);
+            Assert.IsType<NotFoundResult>(actionResult);
+            Assert.Equal(StatusCodes.Status404NotFound, (actionResult as NotFoundResult).StatusCode);
+        }
+        [Fact]
+        public void GetReturnAllNames()
+        {
+            var actionResult = controller.Get() as OkObjectResult;
+            string[] arr = new[] { "Ruslan", "Olga", "Ivan", "Sofia" };
+            Assert.IsType<OkObjectResult>(actionResult);
+            Assert.Equal(StatusCodes.Status200OK, actionResult.StatusCode);
+            Assert.Equal(arr, actionResult.Value as string[]);
+        }
+    }
+}
